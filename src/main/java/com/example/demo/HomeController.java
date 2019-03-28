3cd0b44200b4fe526c5bfd774a68432b2ce62f0f
khadkaraghav <khadkaraghav@gmail.com>
2019-03-28 11:51:07 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    EmailService emailService;
+
+
+    @RequestMapping("/")
+    public String GetIndex(){
+        return "index";
+    }
+
+    @PostMapping("/sendSimpleEmail")
+        public String SendSimpleEmail(){
+        emailService.SendSimpleEmail();
+            return "success";
+        }
+    }
+
